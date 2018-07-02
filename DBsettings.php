5bf574c02f62231e4da300731b4a6dcb8f985293
RobiGit2017 <shen.workgroups@gmail.com>
2018-07-02 10:15:44 +0800

    modify the setting

--- DBsettings.php
+++ DBsettings.php
@@ -6,6 +6,7 @@ class DatabaseSettings
 	{
 		
 		// Database variables
+		/*
 		// Host name
 		$settings['dbhost'] = 'localhost';
 		// Database name
@@ -14,6 +15,12 @@ class DatabaseSettings
 		$settings['dbusername'] = 'gitalesp_Test';
 		// Password
 		$settings['dbpassword'] = '[j9dUrk;SU_q';
+		*/
+		
+		$settings['dbhost'] = '127.0.0.1';
+		$settings['dbname'] = 'test';
+		$settings['dbusername'] = 'user';
+		$settings['dbpassword'] = '';
 		
 		return $settings;
 	}
