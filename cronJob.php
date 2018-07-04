e354130f90ac1729ba5191a65ef4ed29e0c9e3fe
RobiGit2017 <shen.workgroups@gmail.com>
2018-07-04 09:30:53 +0800

    add "multi-process programming"

--- cronJob.php
+++ cronJob.php
@@ -10,17 +10,12 @@ require_once("./database.php");
 require_once("./DataType.php");
 require_once("./nano.php");
 require_once("./ethfan.php");
-
-$db = new db();
-$nano = new nano();
-$ethfan = new ethfan();
-$DataNow = new DataType();
-#$f2pool = new f2pool();
-#$uupool = new uupool();
-#$dwarfpool = new dwarfpool();    ####
+require_once("./f2pool.php");
 
 $WarnRatio = 0.9;
+$process_num = 10;
 
+$db = new db();
 $db->DBConnect();
 $result = $db->selectAll();
 
@@ -41,78 +36,120 @@ $StatusArray = array(
 
 
 #echo(var_dump($ResultArray));
+#$uupool = new uupool();
+#$dwarfpool = new dwarfpool();    ####
 
-$i=0;
-while($i<$result->num_rows){
-	####或許該要先檢查$ResultArray[$i]["Comment"]來確定機台是否不在或是離現線?
-	####不需要 每次重新載一次才知道有沒有重新上線之類的 by:LKM
+$children = array();
 
-	$DataNow->resetData();
-	#echo(var_dump($DataNow));
-	$DataNow->Address = $ResultArray[$i]["Address"];
-	$DataNow->Worker = $ResultArray[$i]["Worker"];
-	$DataNow->Coin = $ResultArray[$i]["Coin"];
-	$SpecifiedHashRate = $ResultArray[$i]["SpecifiedHashRate"];
-	$Status = 0;
-	$APIError = true;
+for($i = 0;$i<$result->num_rows; $i++) {
+    $pid = pcntl_fork();
+    if($pid == -1) {
+        exit(1);
+    } else if ($pid) {
+    	//Parent
+	    //如果超過，就等一秒鐘
+	    //$children = $pid;
+	    //echo (var_dump($children));
+	    echo (var_dump($pid));
+	    while(count($children) >= $process_num){ 
+	    //	echo (count($children));
+	    //	echo (var_dump($children));
+            echo "Maximum children allowed, waiting...<br>"; 
+	    	echo (var_dump($pid));
+            sleep(1); 
+        } 
+	    #echo(var_dump($children));
+   } else {
+   		//array_push($children,$pid);
+   		$db2 = new db();
+		$db2->DBConnect();
 
-	$PoolNow = $ResultArray[$i]["Pool"];
+		$DataNow = new DataType();
+	    $DataNow->resetData();
+		#echo(var_dump($DataNow));
+		$DataNow->Address = $ResultArray[$i]["Address"];
+		$DataNow->Worker = $ResultArray[$i]["Worker"];
+		$DataNow->Coin = $ResultArray[$i]["Coin"];
+		$SpecifiedHashRate = $ResultArray[$i]["SpecifiedHashRate"];
+		$Status = 0;
+		$APIError = true;
 
-	switch ($PoolNow) {
-		case 'nano':
-			##API require
-			$nano->reset();
-			$nano->setBasicData($DataNow->Address,$DataNow->Worker,$DataNow->Coin);
-			$nano->getDataFromPool();
-			$APIError = $nano->ErrorFlag;
+		$PoolNow = $ResultArray[$i]["Pool"];
 
-			if(!$APIError){
-				$DataNow->ReportedHashRate = $nano->ReportedHashRate;
-				$DataNow->HashRate_LongTerm = $nano->HashRate_LongTerm;
-				$DataNow->Balance = $nano->Balance;
-			}
-			break;
-		case 'eth-tw':
-			##API require
-			$ethfan->reset();
-			$ethfan->setBasicData($DataNow->Address,$DataNow->Worker,$DataNow->Coin);
-			$ethfan->getDataFromPool();
-			$APIError = $ethfan->ErrorFlag;
-			
-			if(!$APIError){
-				$DataNow->ReportedHashRate = $ethfan->ReportedHashRate;
-				$DataNow->HashRate_LongTerm = $ethfan->HashRate_LongTerm;
-				$DataNow->Balance = $ethfan->Balance;
-			}
-			break;
-		default:
-			# code...
-			break;
-	}
+		switch ($PoolNow) {
+			case 'nano':
+				$nano = new nano();
+				##API require
+				$nano->reset();
+				$nano->setBasicData($DataNow->Address,$DataNow->Worker,$DataNow->Coin);
+				$nano->getDataFromPool();
+				$APIError = $nano->ErrorFlag;
 
-	#Modify status
-	if(!$APIError){
-		if($DataNow->ReportedHashRate==0){
-			$Status = 4;
-		}elseif($DataNow->ReportedHashRate<$WarnRatio*$SpecifiedHashRate){
-			$Status = 3;
-		}elseif ($DataNow->HashRate_LongTerm<$WarnRatio*$SpecifiedHashRate) {
-			$Status = 2;
+				if(!$APIError){
+					$DataNow->ReportedHashRate = $nano->ReportedHashRate;
+					$DataNow->HashRate_LongTerm = $nano->HashRate_LongTerm;
+					$DataNow->Balance = $nano->Balance;
+				}
+				break;
+			case 'eth-tw':
+				##API require
+				$ethfan = new ethfan();
+				$ethfan->reset();
+				$ethfan->setBasicData($DataNow->Address,$DataNow->Worker,$DataNow->Coin);
+				$ethfan->getDataFromPool();
+				$APIError = $ethfan->ErrorFlag;
+				
+				if(!$APIError){
+					$DataNow->ReportedHashRate = $ethfan->ReportedHashRate;
+					$DataNow->HashRate_LongTerm = $ethfan->HashRate_LongTerm;
+					$DataNow->Balance = $ethfan->Balance;
+				}
+				break;
+			case 'f2pool':
+				##API require
+				$f2pool = new f2pool();
+				$f2pool->reset();
+				$f2pool->setBasicData($DataNow->Address,$DataNow->Worker,$DataNow->Coin);
+				$f2pool->getDataFromPool();
+				$APIError = $f2pool->ErrorFlag;
+				
+				if(!$APIError){
+					$DataNow->ReportedHashRate = $f2pool->ReportedHashRate;
+					$DataNow->HashRate_LongTerm = $f2pool->HashRate_LongTerm;
+					$DataNow->Balance = $f2pool->Balance;
+				}
+				break;
+			default:
+				# code...
+				break;
 		}
-		else{
-			$Status = 1;
+
+		#Modify status
+		if(!$APIError){
+			if($DataNow->ReportedHashRate==0){
+				$Status = 4;
+			}elseif($DataNow->ReportedHashRate<$WarnRatio*$SpecifiedHashRate){
+				$Status = 3;
+			}elseif ($DataNow->HashRate_LongTerm<$WarnRatio*$SpecifiedHashRate) {
+				$Status = 2;
+			}
+			else{
+				$Status = 1;
+			}
+		}else{
+			#API Error!!!
+			$Status = 5;
 		}
-	}else{
-		#API Error!!!
-		$Status = 5;
-	}
 
-	$timeNow = date("Y-m-d H:i:s");
+		$timeNow = date("Y-m-d H:i:s");
 
-	$cmd= "UPDATE GPU SET Balance = '$DataNow->Balance' , Reported = '$DataNow->ReportedHashRate' , 24Hrs = '$DataNow->HashRate_LongTerm' , Status = '$StatusArray[$Status]' , UpdateTime = '$timeNow' WHERE Worker = '$DataNow->Worker' AND Address= '$DataNow->Address'";
-	echo($cmd."<br>");
-	$db->query($cmd);
-	
-	$i+=1;
+		$cmd= "UPDATE GPU SET Balance = '$DataNow->Balance' , Reported = '$DataNow->ReportedHashRate' , 24Hrs = '$DataNow->HashRate_LongTerm' , Status = '$StatusArray[$Status]' , UpdateTime = '$timeNow' WHERE Worker = '$DataNow->Worker' AND Address= '$DataNow->Address'";
+		echo($cmd."<br>");
+		$db2->query($cmd);
+
+		//unset($children[array_search($pid,$children)]);
+    	break; //直接出迴圈
+   }
 }
+
 ?>
